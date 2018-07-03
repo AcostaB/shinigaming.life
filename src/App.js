bbca5abb6f5cdd6308dafd049d521a35c3322a1c
Bryand Acosta <b_acosta@icloud.com>
2018-07-02 23:49:49 -0400

    Work on main page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="header">
+          Welcome to
+        </div>
+        <div className="logo"/>
+        <div className="title">
+          <div className="title-part">SHINI</div>
+          <div className="title-part">GAMING.</div>
+          <div className="title-part">LIFE</div>
+        </div>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
