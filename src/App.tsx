d4e9a804ddc0c91954df2996a7187ee603e267be
Bryand <acostab@infinity-software.com>
2018-07-11 15:07:54 -0400

    Removing the package folder for sake of simplicity

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,44 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+// import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './App.css';
-import testView from "./Views/testView";
+// import testView from "./Views/testView";
 
 // TODO: Added a symbolic link in order to be able to import packages from the children folders. This seems a bit odd tho. Research other options.
 // TODO need to improve on the symbolic link. Should not point to everything, and instead point to only a few necessary things like a build output. 
 
-import App from "DnD/src/App";
+// import App from "DnD/src/App";
 
 // TODO: research. React-router should not be necessary. Only react-router-dom
 // src: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
 
-class App2 extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-      <BrowserRouter>
-        <Switch>
+      placeholder
+      {/* <BrowserRouter>
+        <Switch> */}
           {/* // TODO The order of the links seems to make a difference. Research this.  */}
           {/* Routes finds anything that matches description. Needs "exact" src: https://blog.pusher.com/getting-started-with-react-router-v4/ */}
-          <Route exact path="/" component={testView}/>
+          {/* <Route exact path="/" component={testView}/>
           <Route exact path="/DnD" component={App}/>
         </Switch>
-      </BrowserRouter>
+      </BrowserRouter> */}
       </div>
     );
   }
 }
 
-export default App2;
+// import React, { Component } from 'react';
+// import {Dashboard} from '../src/Containers/Dashboard';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Dashboard/>
+//     );
+//   }
+// }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, Store } from 'redux';
 import rootReducer from './Reducers/dndReducers';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import initialState from "./Models/InitialState";
 
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
 const store: Store = createStore(rootReducer, initialState as any, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__());
-// , (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 
 console.log("Store state:", store.getState());
 
