a87a92c5d38c37a73cf960800eb354195b776047
Bryand Acosta <b_acosta@icloud.com>
2018-08-11 17:35:47 -0400

    dashboard - styled components refactor

--- src/Containers/Dashboard.tsx
+++ src/Containers/Dashboard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from '../Components/Header/Header';
+import React from "react";
+import Header from "../Components/Header/Header";
 import "./Dashboard.css";
 
 import AbilitiesPanel from "../Components/Panels/AbilitiesPanel/AbilitiesPanel";
@@ -9,51 +9,110 @@ import LimitedUsesPanel from "../Components/Panels/LimitedUsesPanel/LimitedUsesP
 import PassivesPanel from "../Components/Panels/PassivesPanel/PassivesPanel";
 import SkillsPanel from "../Components/Panels/SkillsPanel/SkillsPanel";
 
-import {Currency} from "../Models/Currency";
+import BodyBackground from "../Assets/body-background.jpg";
+import HeaderBackground from "../Assets/header-background.png";
+
+import styled from "styled-components";
+import { Currency } from "../Models/Currency";
 
 interface IState {
-  remainingHealth: number, 
-  remainingLimitedUses: {[limitedUsesName: string]: number},
-  currencyTabActive: boolean,
-  currency: Currency,
-  addNewItemExpanded: boolean
+  remainingHealth: number;
+  remainingLimitedUses: { [limitedUsesName: string]: number };
+  currencyTabActive: boolean;
+  currency: Currency;
+  addNewItemExpanded: boolean;
 }
 
+// TODO need to fix this any
 export class Dashboard extends React.Component<{}, IState> {
-  render() {
+  render(): any {
     return (
-      <div className="App-DnD">
-        <div className="App-DnD-header">
-          <div className="App-DnD-header-content">
-            <Header/>
-          </div>
-        </div>
-        <div className="App-DnD-body">
-          <div className="App-DnD-body-content">
-            <div className="dashboard">
+      <DnD>
+        <DnDHeader>
+          <HeaderContent>
+            <Header />
+          </HeaderContent>
+        </DnDHeader>
+        <DnDBody>
+          <BodyContent>
+            <Dashboard>
               {/* TODO clean this css up with styled components or something. */}
-              <div className="dashboard-panel">
-                <AbilitiesPanel/>
-              </div>
-              <div className="dashboard-panel">
-                <SkillsPanel/>
-              </div>
-              <div className="dashboard-panel">
-                <LimitedUsesPanel/>          
-              </div>
-              <div className="dashboard-panel">
-                <PassivesPanel/>
-              </div>
-              <div className="dashboard-panel">
-                <AttacksPanel/>
-              </div>
-              <div className="dashboard-panel">
-                <InventoryPanel/> 
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              <Panel>
+                <AbilitiesPanel />
+              </Panel>
+              <Panel className="dashboard-panel">
+                <SkillsPanel />
+              </Panel>
+              <Panel className="dashboard-panel">
+                <LimitedUsesPanel />
+              </Panel>
+              <Panel className="dashboard-panel">
+                <PassivesPanel />
+              </Panel>
+              <Panel className="dashboard-panel">
+                <AttacksPanel />
+              </Panel>
+              <Panel className="dashboard-panel">
+                <InventoryPanel />
+              </Panel>
+            </Dashboard>
+          </BodyContent>
+        </DnDBody>
+      </DnD>
     );
   }
 }
+
+const DnD = styled.div`
+   {
+    background-image: url(${BodyBackground});
+    background-position-x: center, initial;
+    background-position-y: 120px;
+    background-size: initial, initial;
+    background-repeat: no-repeat, initial;
+    background-attachment: initial, initial;
+    background-origin: initial, initial;
+    background-clip: initial, initial;
+    background-color: rgb(249, 249, 249);
+    height: 100%;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    box-sizing: border-box;
+  }
+`;
+
+const DnDHeader = styled.div`
+  background: url(${HeaderBackground});
+  background-position-y: -238px;
+`;
+
+// TODO: the next two share properties. Need to figure out how to share them so I dont repeat.
+const HeaderContent = styled.div`
+  width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 50px;
+  padding-right: 50px;
+`;
+
+const BodyContent = styled.div`
+  width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+// Example of media query.
+const DnDBody = styled.div`
+  margin: 20px auto;
+
+  @media (max-width: 800px) {
+    padding: 20px 10px 0 20px !important;
+  }
+`;
+
+const Panel = styled.div`
+  width: 33%;
+  display: inline-block;
+  padding: 0 5px 15px 5px;
+  box-sizing: border-box;
+`;
