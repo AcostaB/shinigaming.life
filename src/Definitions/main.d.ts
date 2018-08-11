48b9eada89bbad4cac2f797f4674836681ac2da5
Bryand Acosta <b_acosta@icloud.com>
2018-08-11 17:29:57 -0400

    Added definitions for image imports

---
+++ src/Definitions/main.d.ts
@@ -0,0 +1,6 @@
+// TODO this is a hacky fix for the issue of importing a png file, shinigaming.png. Sources:
+// https://github.com/Microsoft/TypeScript-React-Starter/issues/12
+// https://github.com/Microsoft/TypeScript/issues/21344
+
+declare module "*.png";
+declare module "*.jpg";
