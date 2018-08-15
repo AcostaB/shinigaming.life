1763d3e567a7549c242c90b823057af9c9d556b7
Bryand <acostab@infinity-software.com>
2018-08-15 11:31:38 -0400

    Some styling for the linked in feedback.

--- src/Components/LinkedInFeedback.tsx
+++ src/Components/LinkedInFeedback.tsx
@@ -22,65 +22,44 @@ export default LinkedInFeedback;
 const LinkedInFeedbackContainer = styled.div`
   text-align: left;
   font-family: "AntipastoPro-Hairline";
+  color: white;
+  font-size: 14px;
 `;
 
 const LeftSection = styled.div`
   display: inline-block;
+  width: 20%;
 `;
 
 const Name = styled.div`
   align-items: center;
-  font-size: 30px;
   color: #4bb9cd;
   background: linear-gradient(-90deg,  red,  green);
 `;
 
 const Position = styled.div`
-  color: white;
   letter-spacing: 2px;
-  font-size: 18px;
-  margin-top: 5px;
-  margin-left: auto;
-  margin-right: auto;
   line-height: 1.2;
 `;
 
 const Relationship = styled.div`
-  color: white;
   letter-spacing: 2px;
-  font-size: 16px;
-  margin-top: 5px;
-  margin-left: auto;
-  margin-right: auto;
   line-height: 1.2;
 `;
 
 const Location = styled.ul`
-  display: inline-block;
-  margin: 0 10px 0 10px;
   font-size: 16px;
-  color: white;
   letter-spacing: 1px;
-  margin-top: 10px;
   line-height: 1.5;
 `;
 
 const DateString = styled.ul`
-  display: inline-block;
-  margin: 0 10px 0 10px;
-  font-size: 16px;
-  color: white;
   letter-spacing: 1px;
-  margin-top: 10px;
   line-height: 1.5;
 `;
 
 const Feedback = styled.ul`
   display: inline-block;
-  margin: 0 10px 0 10px;
-  font-size: 16px;
-  color: white;
   letter-spacing: 1px;
-  margin-top: 10px;
-  line-height: 1.5;
+  width: 60%;
 `;
